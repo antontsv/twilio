215dc30cbbb9d1c22d4b866c7c192aca713d2cec
Anton Tsviatkou <a.tsviatkou@gmail.com>
2017-06-25 20:29:33 -0700

    Make use of Voicemail twimlet and basic Twilio voice APIs

---
+++ main.go
@@ -0,0 +1,116 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"os"
+)
+
+var envVarNames = []string{"TWILIO_NUMBER", "TARGET_NUMBER", "VOICEMAIL_EMAIL"}
+
+var envVars map[string]string
+
+func main() {
+	addr := os.Getenv("BIND_ADDRESS")
+	if addr == "" {
+		addr = "localhost:8080"
+	}
+	envVars = make(map[string]string, len(envVarNames))
+	for _, name := range envVarNames {
+		val := os.Getenv(name)
+		if val != "" {
+			envVars[name] = val
+		}
+	}
+	http.HandleFunc("/incoming-call", StraightToVoiceMail)
+	http.HandleFunc("/process-recording", HandleRecordCallback)
+	log.Println(fmt.Sprintf("About to start listen on %s", addr))
+	log.Fatal(http.ListenAndServe(addr, nil))
+}
+
+func sendTwiML(w http.ResponseWriter, twiML string) {
+	tmpl, err := template.New("twiMLResponse").Parse(twiML)
+	if err != nil {
+		log.Printf("error creating TwiML template: %v\n", err)
+	}
+
+	w.Header().Add("Content-Type", "text/xml; charset=utf-8")
+
+	err = tmpl.Execute(w, envVars)
+	if err != nil {
+		log.Printf("error while executing TwiML template: %v\n", err)
+	}
+}
+
+// StraightToVoiceMail uses twimlet to record a message and send it over by email
+func StraightToVoiceMail(w http.ResponseWriter, req *http.Request) {
+	xml := `<?xml version="1.0" encoding="UTF-8"?>
+<Response>
+	<Redirect method="GET">http://twimlets.com/voicemail?Email={{ .VOICEMAIL_EMAIL }}&amp;Message=Please+Leave+A+Message</Redirect>
+</Response>
+`
+	sendTwiML(w, xml)
+}
+
+// HandleIncomingCall serves XML to kick off incoming call
+func HandleIncomingCall(w http.ResponseWriter, req *http.Request) {
+	responseFormat := `<?xml version="1.0" encoding="UTF-8"?>
+<Response>
+    <Say voice="alice" language="en-US">Please leave a message</Say>
+	<Sms from="{{ .TWILIO_NUMBER }}" to="{{ .TARGET_NUMBER }}">Call from %s, %s, %s.</Sms>
+	<Record maxLength="300" action="/process-recording"></Record>
+	<Say voice="alice" language="en-US">I may not give you a call back. Please leave a message.</Say>
+	<Record maxLength="300" action="/process-recording"></Record>
+	<Say voice="alice" language="en-US">Ok, as you wish, good bye</Say>
+</Response>
+`
+	city := getRequestParam(req, "FromCity", "Unknown city")
+	fromNumber := getRequestParam(req, "From", "Unknown number")
+	country := getRequestParam(req, "FromCountry", "Unknown country")
+
+	xml := fmt.Sprintf(responseFormat, city, fromNumber, country)
+	sendTwiML(w, xml)
+}
+
+// HandleRecordCallback handles Twilio callback with a recorded message
+func HandleRecordCallback(w http.ResponseWriter, req *http.Request) {
+	responseFormat := `<?xml version="1.0" encoding="UTF-8"?>
+<Response>
+	<Say voice="alice" language="en-US">Thank you, you should hear back soon</Say>
+	<Sms from="{{ .TWILIO_NUMBER }}" to="{{ .TARGET_NUMBER }}">You have got a twilio call from %s, %s, %s</Sms>
+</Response>
+`
+	if err := req.ParseForm(); err != nil {
+		log.Printf("Failed to parse form %v\n", err)
+	}
+	city := getRequestParam(req, "FromCity", "Unknown city")
+	fromNumber := getRequestParam(req, "From", "Unknown number")
+	country := getRequestParam(req, "FromCountry", "Unknown country")
+	_ = getRequestParam(req, "RecordingUrl", "no message")
+
+	xml := fmt.Sprintf(responseFormat, city, fromNumber, country)
+	sendTwiML(w, xml)
+}
+
+func getRequestParam(req *http.Request, paramName, defaultValue string) string {
+	value := paramGet(req)(paramName)
+	if value == "" && defaultValue != "" {
+		return defaultValue
+	}
+
+	return value
+}
+
+func paramGet(req *http.Request) func(string) string {
+	if req.Method == "GET" {
+		return func(key string) string {
+			return req.URL.Query().Get(key)
+		}
+	}
+	return func(key string) string {
+		return req.Form.Get(key)
+	}
+
+}
